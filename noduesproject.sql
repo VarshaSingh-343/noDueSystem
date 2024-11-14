c71aa194f1e6db03a95f08e46a3172e910924cb4
Varsha Singh <varshaofficial343@gmail.com>
2024-11-14 10:38:14 +0530

    sql commit

--- noduesproject.sql
+++ noduesproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2024 at 09:04 PM
+-- Generation Time: Nov 14, 2024 at 06:06 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
